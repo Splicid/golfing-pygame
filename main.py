a2f1945c13a3cb71b69d9f0217a2229747d037a0
Luis Abreu <luis1abreu11@gmail.com>
2024-06-23 19:16:05 -0400

    start

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-import pygame
+import gym
+import numpy as np
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+env = gym.make('CartPole-v1', render_mode="human")
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+state = env.reset()
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+for _ in range(1000):
+    action = env.action_space.sample()
+    
+    result = env.step(action)
+    
+    if len(result) == 5:
+        state, reward, terminated, truncated, info = result
+        done = terminated or truncated
+    else:
+        state, reward, done, info = result
 
-    # RENDER YOUR GAME HERE
+    env.render()
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+    if done:
+        state = env.reset()
 
-    clock.tick(60)  # limits FPS to 60
-
-pygame.quit()
+env.close()
