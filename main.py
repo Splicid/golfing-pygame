e9420ba86aa1743f49a3896e7c5116499bb245aa
Luis Abreu <luis1abreu11@gmail.com>
2024-07-02 01:18:36 -0400

    Working on drawing

--- main.py
+++ main.py
@@ -3,28 +3,30 @@ import pygame
 import pygame_gui
 import sys
 
-
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
 BG_COLOR = (0, 0, 0)  
 
-class draw():
+class Draw:
     def __init__(self) -> None:
+        self.SCREEN_WIDTH = 1280
+        self.SCREEN_HEIGHT = 720
         pygame.init()
 
         pygame.display.set_caption("Drawing")
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), pygame.RESIZABLE)
         self.clock = pygame.time.Clock()
-        self.manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.manager = pygame_gui.UIManager((self.SCREEN_WIDTH, self.SCREEN_HEIGHT),'ui-themes/buttonTheme.json')
         self.running = True
 
     def layout(self):
-        layout = pygame_gui.elements.UIPanel(relative_rect=pygame.Rect((0, 0), (160, SCREEN_HEIGHT)),
+        self.ui = pygame_gui.elements.UIPanel(relative_rect=pygame.Rect((0, 0), (160, self.SCREEN_HEIGHT)),
                                              manager=self.manager)
-        hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((5, 50), (50, 50)),
+        
+        self.hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((30, 50), (50, 50)),
                                                 text='Red',
-                                                manager=self.manager)
-        #self.manager.set_visual_debug_mode(True)
+                                                manager=self.manager,
+                                                container=self.ui
+                                                )
+        
 
     def run(self):
         self.layout()
@@ -43,20 +45,27 @@ class draw():
                 self.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    self.running = False
+                    #self.running = False
+                    self.manager.set_visual_debug_mode(True)
+            elif event.type == pygame.VIDEORESIZE:
+                self.SCREEN_WIDTH, self.SCREEN_HEIGHT = event.size
+                self.manager.set_window_resolution((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+                self.ui.set_dimensions((160, self.SCREEN_HEIGHT))
+            elif event.type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == self.hello_button:
+                    print('Clicked')
             self.manager.process_events(event)
 
     def update(self):
-        pass
+        time_delta = self.clock.tick(240) / 1000.0
+        self.manager.update(time_delta)
     
     def draw(self):
         self.screen.fill(BG_COLOR)
-        self.manager.update(self.clock.tick(120))
         self.manager.draw_ui(self.screen)
         pygame.display.flip()
     
 
 if __name__ == "__main__":
-    
-    game = draw()
+    game = Draw()
     game.run()
